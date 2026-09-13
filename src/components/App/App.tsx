c2c99653e1484e4d5ed3142fb0c3f3981044a0b4
yuriilizan95 <yuriilizan@gmail.com>
2026-09-13 10:37:19 +0200

    make photogallery

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,11 @@ import Section from "../Section/Section";
 import Container from "../Container/Container";
 import { getPhotos } from "../../services/photos";
 import Form from "../Form/Form";
+import toast, { Toaster } from "react-hot-toast";
+import PhotosGallery from "../PhotosGallery/PhotosGallery";
+import { useState } from "react";
+import type { Photo } from "../../types/photo";
+
 // import { useEffect } from "react";
 
 export default function App() {
@@ -12,17 +17,24 @@ export default function App() {
   //   };
   //   handleSearch();
   // }, []);
+  const [photos, setPhotos] = useState<Photo[]>([])
 
   const handleSubmit = async (query: string) => {
     const response = await getPhotos(query);
-    console.log(response);
+    if (response.length === 0) {
+      toast.error("No photos found...")
+      return
+    }
+    setPhotos(response);
   };
 
   return (
     <>
       <Section>
         <Container>
+          <Toaster/>
           <Form onSubmit={handleSubmit} />
+          <PhotosGallery photos={photos}/>
         </Container>
       </Section>
     </>
